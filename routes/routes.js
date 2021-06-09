ee012c9279d2a4a9bd01db24e0a0dbf022117ed2
argelignacio <iargel@fi.uba.com>
2021-06-09 15:37:27 -0300

    aniade consulta por id

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const peliculas = require('../peliculas.json')
+const { peliculas } = require("../peliculas.json");
 
 router.get("/peliculas", (req, res) => {
   res.json(peliculas);
+});
+var control = false;
+router.get("/peliculas/:id", (req, res) => {
+  const id = req.params.id;
+  for (var i = 0; i < peliculas.length; i++) {
+    if (peliculas[i]["Identificador"] == parseInt(id)) {
+      res.json(peliculas[i]);
+      control= true;
+    }
+  }
+  if (control == false ){
+    res.send("No encontrado");
+  }
+  
 });
 
 module.exports = router;
