6fa00534a73debbc1d1b89a9eb8289125cfba6a7
argelignacio <iargel@fi.uba.com>
2021-06-09 12:23:56 -0300

    /peliculas devuelve json

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
+const routes = require("./routes/routes");
 
+const app = express();
 
-const app = express()
-
-
-
+app.use(routes);
 
 app.listen(3000, () => {
-    console.log('funcionando')
-})
-
+  console.log("funcionando");
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const router = express.Router();
+
+const peliculas = require('../peliculas.json')
+
+router.get("/peliculas", (req, res) => {
+  res.json(peliculas);
+});
+
+module.exports = router;
